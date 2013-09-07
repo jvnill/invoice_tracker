d050c4edd08873e3cdae37d0ccc71b2f44ece1ff
Jim Ruther Nill <jim@netengine.com.au>
2013-09-08 02:06:11 +0800

    use gem to provide wkhtmltopdf binary

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
 gem 'wicked_pdf'
 
+group :production do
+  gem 'wkhtmltopdf-heroku'
+end
+
 group :doc do
   gem 'sdoc', require: false
 end
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,1 +1,3 @@
-WickedPdf.config = { exe_path: Rails.root.join('vendor', 'bin', Rails.env.production? ? 'wkhtmltopdf-i386' : 'wkhtmltopdf').to_s }
+unless Rails.env.production?
+  WickedPdf.config = { exe_path: Rails.root.join('vendor', 'bin', 'wkhtmltopdf').to_s }
+end
