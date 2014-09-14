bad50e1ae379860cdef665184583ab809b34cbf2
Jim Ruther Nill <jvnill@gmail.com>
2014-09-14 22:08:19 +0800

    fix ui of top navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def navlink(text, url, active, link_options = {})
+    content_tag :li, link_to(text, url, link_options), class: ('active' if active)
+  end
+
   def subnavlink(icon, text, url, options = {})
     link_to url, options.merge(class: 'flat') do
       content_tag(:span, '', class: "icon-#{icon} with-text") + text
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
+  describe '#navlink' do
+    it { expect(helper.navlink('text', 'url', true)).to eql('<li class="active"><a href="url">text</a></li>') }
+    it { expect(helper.navlink('text', 'url', false)).to eql('<li><a href="url">text</a></li>') }
+    it { expect(helper.navlink('text', 'url', false, class: 'foo')).to eql('<li><a class="foo" href="url">text</a></li>') }
+  end
+
   describe '#subnavlink' do
     let!(:output) { helper.subnavlink('icon', 'text', 'url') }
 
