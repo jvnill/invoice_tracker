39420a365a199aadaed59019f37121dec22785bf
Jim Ruther Nill <jvnill@gmail.com>
2014-09-14 22:39:41 +0800

    move subnav links to topnav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
-  def navlink(text, url, active, link_options = {})
-    content_tag :li, link_to(text, url, link_options), class: ('active' if active)
-  end
-
-  def subnavlink(icon, text, url, options = {})
-    link_to url, options.merge(class: 'flat') do
-      content_tag(:span, '', class: "icon-#{icon} with-text") + text
-    end
+  def navlink(text, url, link_options = {})
+    content_tag :li, link_to(text, url, link_options), class: ('active' if current_page?(url))
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 describe ApplicationHelper do
   describe '#navlink' do
-    it { expect(helper.navlink('text', 'url', true)).to eql('<li class="active"><a href="url">text</a></li>') }
-    it { expect(helper.navlink('text', 'url', false)).to eql('<li><a href="url">text</a></li>') }
-    it { expect(helper.navlink('text', 'url', false, class: 'foo')).to eql('<li><a class="foo" href="url">text</a></li>') }
-  end
-
-  describe '#subnavlink' do
-    let!(:output) { helper.subnavlink('icon', 'text', 'url') }
-
-    it { expect(output).to eql('<a class="flat" href="url"><span class="icon-icon with-text"></span>text</a>') }
+    it { expect(helper.navlink('text', '')).to eql('<li class="active"><a href="">text</a></li>') }
+    it { expect(helper.navlink('text', 'url')).to eql('<li><a href="url">text</a></li>') }
+    it { expect(helper.navlink('text', 'url', class: 'foo')).to eql('<li><a class="foo" href="url">text</a></li>') }
   end
 end
