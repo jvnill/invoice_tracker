77e53255947c78b435973125b7eb41186f26f986
Jim Ruther Nill <jvnill@gmail.com>
2014-09-20 08:56:52 +0800

    use a helper to generate the project select dropdown in new invoice page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def navlink(text, url, link_options = {})
     content_tag :li, link_to(text, url, link_options), class: ('active' if current_page?(url))
   end
+
+  def project_select(builder, projects)
+    builder.select :project_id, grouped_options_for_select(grouped_project_options(projects)), { include_blank: true }
+  end
+
+  def grouped_project_options(projects)
+    projects
+      .select('clients.name AS client_name, projects.name, projects.id')
+      .order('clients.name, projects.name')
+      .each_with_object({}) do |project, hash|
+
+      hash[project.client_name] ||= []
+      hash[project.client_name]  << [project.name, project.id]
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,30 @@ describe ApplicationHelper do
     it { expect(helper.navlink('text', 'url')).to eql('<li><a href="url">text</a></li>') }
     it { expect(helper.navlink('text', 'url', class: 'foo')).to eql('<li><a class="foo" href="url">text</a></li>') }
   end
+
+  describe '#project_select' do
+    let!(:user) { create(:user) }
+
+    before { expect(helper).to receive(:grouped_project_options).and_return({ 1 => [2] }) }
+
+    it 'should return the correct html markup' do
+      form_for(Invoice.new) do |f|
+        expect(helper.project_select(f, user.projects)).to eql("<select id=\"invoice_project_id\" name=\"invoice[project_id]\"><option value=\"\"></option>\n<optgroup label=\"1\"><option value=\"2\">2</option></optgroup></select>")
+      end
+    end
+  end
+
+  describe '#grouped_project_options' do
+    let!(:user)     { create(:user) }
+
+    let!(:client1)  { create(:client, name: 'A', user: user) }
+    let!(:project1) { create(:project, name: 'A', client: client1) }
+    let!(:project2) { create(:project, name: 'B', client: client1) }
+
+    let!(:client2)  { create(:client, name: 'B', user: user) }
+    let!(:project3) { create(:project, name: 'D', client: client2) }
+    let!(:project4) { create(:project, name: 'C', client: client2) }
+
+    it { expect(helper.grouped_project_options(user.projects)).to eql({ client1.name => [[project1.name, project1.id], [project2.name, project2.id]], client2.name => [[project4.name, project4.id], [project3.name, project3.id]] }) }
+  end
 end
