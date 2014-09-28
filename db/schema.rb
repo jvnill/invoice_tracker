d897d39c0576b1e28e493d9ec662adc922f33f27
Jim Ruther Nill <jvnill@gmail.com>
2014-09-28 09:24:33 +0800

    add index to user auth_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923123537) do
+ActiveRecord::Schema.define(version: 20140928011922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,5 +90,7 @@ ActiveRecord::Schema.define(version: 20140923123537) do
     t.datetime "updated_at"
     t.string   "auth_token"
   end
+
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", using: :btree
 
 end
