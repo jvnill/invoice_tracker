b61283395cc83f53c6d51d65089ac5c112a098c4
Jim Ruther Nill <jvnill@gmail.com>
2014-10-03 17:22:29 +0800

    generate authentications table

---
+++ db/migrate/20141003092148_create_authentications.rb
@@ -0,0 +1,11 @@
+class CreateAuthentications < ActiveRecord::Migration
+  def change
+    create_table :authentications do |t|
+      t.string :uid
+      t.string :provider
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928012250) do
+ActiveRecord::Schema.define(version: 20141003092148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: true do |t|
+    t.string   "uid"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
   create_table "clients", force: true do |t|
     t.string   "name"
