1e520989bc82d63398ff6c1968947e5d2457405b
Jim Ruther Nill <jvnill@gmail.com>
2016-03-07 22:14:43 +0800

    Revert "add google auth but hide link"
    This reverts commit 47d8cdabc80e611f1125930dbf73143979e8de1a.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,        ENV['GITHUB_INVOICE_TRACKER_KEY'],   ENV['GITHUB_INVOICE_TRACKER_SECRET']
-  provider :linkedin,      ENV['LINKEDIN_INVOICE_TRACKER_KEY'], ENV['LINKEDIN_INVOICE_TRACKER_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_INVOICE_TRACKER_KEY'],   ENV['GOOGLE_INVOICE_TRACKER_SECRET']
+  provider :github,   ENV['GITHUB_INVOICE_TRACKER_KEY'],   ENV['GITHUB_INVOICE_TRACKER_SECRET']
+  provider :linkedin, ENV['LINKEDIN_INVOICE_TRACKER_KEY'], ENV['LINKEDIN_INVOICE_TRACKER_SECRET']
 end
